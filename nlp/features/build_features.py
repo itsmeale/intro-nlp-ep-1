09ebb97d28643b42ccfec6f4a4b1876a782e1835
Ale Farias <mail.alefarias@gmail.com>
2020-10-03 00:07:59 -0300

    Adds vectorizers

--- nlp/features/build_features.py
+++ nlp/features/build_features.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
-import re
-from typing import Dict, List
+from typing import Dict, List, Tuple
 from pathlib import Path
 
 import pandas as pd
-from unidecode import unidecode
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 from nlp.toolbox.datasets import write_dataset, load_raw_dataset
 from nlp.toolbox.preprocessing import (
@@ -21,7 +20,7 @@ TFIDF_FILEPATH = Path("data", "interim", "tfidf.parquet")
 
 DATASET_TOPIC: Dict = {
     RAW_ABORTION_FILE: "abortion",
-    RAW_GUN_FILE: "gun",
+    RAW_GUN_FILE: "gun-control",
 }
 
 
@@ -30,6 +29,8 @@ def load_datasets():
     for dataset_path, topic in DATASET_TOPIC.items():
         temp_df = load_raw_dataset(dataset_path)
         temp_df["topic"] = topic
+        temp_df["opinion"] = temp_df[topic]
+        temp_df.drop(columns=[topic], inplace=True)
         datasets.append(temp_df)
     return pd.concat(datasets)
 
@@ -42,21 +43,41 @@ def preprocess_dataframe(
         .pipe(remove_accents, document_col)
         .pipe(remove_punctuation, document_col)
         .pipe(collapse_spaces, document_col)
+        .reset_index()
+        .drop(columns=["index"])
     )
 
 
+def vectorize_text_data(
+    df: pd.DataFrame, document_col: str, Vectorizer, ngram_range: Tuple = (1, 1)
+) -> pd.DataFrame:
+    vectorizer = Vectorizer(ngram_range=ngram_range)
+    features = vectorizer.fit_transform(df[document_col].values)
+    features_df = pd.DataFrame(
+        features.todense(), columns=vectorizer.get_feature_names()
+    )
+    return pd.concat([features_df, df], axis=1)
+
+
 def main():
+    documents_col: str = "text"
     df: pd.DataFrame = load_datasets()
-    preprocessed_df: pd.DataFrame = preprocess_dataframe(df)
+    preprocessed_df: pd.DataFrame = preprocess_dataframe(df, documents_col)
 
-    # TODO: implement this
-    # bag_of_words_df: pd.DataFrame = generate_bow(preprocessed_df)
+    bag_of_words_df: pd.DataFrame = vectorize_text_data(
+        df=preprocessed_df,
+        document_col=documents_col,
+        Vectorizer=CountVectorizer,
+    )
 
-    # TODO: implement this
-    # tfidf_df: pd.DataFrame = generate_tfidf(preprocessed_df)
+    tfidf_df: pd.DataFrame = vectorize_text_data(
+        df=preprocessed_df,
+        document_col=documents_col,
+        Vectorizer=TfidfVectorizer,
+    )
 
-    # write_dataset(bag_of_words_df, BOW_FILEPATH)
-    # write_dataset(tfidf_df, TFIDF_FILEPATH)
+    write_dataset(bag_of_words_df, BOW_FILEPATH)
+    write_dataset(tfidf_df, TFIDF_FILEPATH)
 
 
 if __name__ == "__main__":
--- nlp/toolbox/preprocessing.py
+++ nlp/toolbox/preprocessing.py
@@ -6,6 +6,7 @@ from unidecode import unidecode
 
 def remove_punctuation(df: pd.DataFrame, document_col):
     new_df = df.copy()
+    del df
     removal = lambda s: re.sub("[^A-Za-z0-9\s]", "", s)
     new_df[document_col] = new_df[document_col].apply(removal)
     return new_df
@@ -13,18 +14,21 @@ def remove_punctuation(df: pd.DataFrame, document_col):
 
 def remove_accents(df: pd.DataFrame, document_col):
     new_df = df.copy()
+    del df
     new_df[document_col] = new_df[document_col].apply(unidecode)
     return new_df
 
 
 def to_lowercase(df: pd.DataFrame, document_col):
     new_df = df.copy()
+    del df
     new_df[document_col] = new_df[document_col].str.lower()
     return new_df
 
 
 def collapse_spaces(df: pd.DataFrame, document_col):
     new_df = df.copy()
+    del df
     collapse = lambda s: re.sub(" +", " ", s)
     new_df[document_col] = new_df[document_col].apply(collapse)
     new_df[document_col] = new_df[document_col].str.strip()
