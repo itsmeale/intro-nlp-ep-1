3ffb14d78c24212117b481d58f9bc2f253b47606
Ale Farias <mail.alefarias@gmail.com>
2020-10-03 00:13:53 -0300

    Adds decorator to reuse code on preprocessing

--- nlp/toolbox/preprocessing.py
+++ nlp/toolbox/preprocessing.py
@@ -4,32 +4,37 @@ import pandas as pd
 from unidecode import unidecode
 
 
+def preprocess_on_copy(function):
+    def decorate_function(df, document_col):
+        new_df = df.copy()
+        del df
+        return function(new_df, document_col)
+
+    return decorate_function
+
+
+@preprocess_on_copy
 def remove_punctuation(df: pd.DataFrame, document_col):
-    new_df = df.copy()
-    del df
     removal = lambda s: re.sub("[^A-Za-z0-9\s]", "", s)
-    new_df[document_col] = new_df[document_col].apply(removal)
-    return new_df
+    df[document_col] = df[document_col].apply(removal)
+    return df
 
 
+@preprocess_on_copy
 def remove_accents(df: pd.DataFrame, document_col):
-    new_df = df.copy()
-    del df
-    new_df[document_col] = new_df[document_col].apply(unidecode)
-    return new_df
+    df[document_col] = df[document_col].apply(unidecode)
+    return df
 
 
+@preprocess_on_copy
 def to_lowercase(df: pd.DataFrame, document_col):
-    new_df = df.copy()
-    del df
-    new_df[document_col] = new_df[document_col].str.lower()
-    return new_df
+    df[document_col] = df[document_col].str.lower()
+    return df
 
 
+@preprocess_on_copy
 def collapse_spaces(df: pd.DataFrame, document_col):
-    new_df = df.copy()
-    del df
     collapse = lambda s: re.sub(" +", " ", s)
-    new_df[document_col] = new_df[document_col].apply(collapse)
-    new_df[document_col] = new_df[document_col].str.strip()
-    return new_df
+    df[document_col] = df[document_col].apply(collapse)
+    df[document_col] = df[document_col].str.strip()
+    return df
