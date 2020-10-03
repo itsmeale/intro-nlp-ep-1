93cc5704e739e092195670d5534d9c54bc4d51f7
Ale Farias <mail.alefarias@gmail.com>
2020-10-02 22:36:24 -0300

    Git init

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="natural-language-processing-ep1",
+    description="implementation of classifier for small dataset about moral perspectives",
+    version="0.0.0",
+    packages=find_packages()
+)
---
+++ src/features/build_features.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+
+if __name__ == "__main__":
+    df: pd.DataFrame = pd.DataFrame({"test": [1, 2, 3]})
+    print(df)
